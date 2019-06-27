7a38a7ec4f9c7573777d16c5284506dc7574789a
Eliza Munroe <era3munroe@gmail.com>
2019-06-27 21:25:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  arrays! = array.pop("arrays!")
+  arrays! = array.pop
   p array
 end
 
